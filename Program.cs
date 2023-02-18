1ecd971f6fb00d1f94d8d393cae464b67e7d8809
Murat.Can <muratt.can.94@gmail.com>
2023-02-18 17:53:55 +0300

    if_else ornek

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace if_else
+{
+    class Program
+    {
+        static void Main (string[] args)
+        {
+            int time = DateTime.Now.Hour;
+
+            if(time >= 6 && time < 11)
+            Console.WriteLine("Günaydın");
+            else if (time <= 18)
+            Console.WriteLine("İyi Akşamlar");
+            else
+            Console.WriteLine("İyi Gceler");
+
+            int kilo = 88;
+            string oneri = kilo <= 70 ? "Kilon Gayet iyi" : "Kilo Vermelisin";
+            Console.WriteLine(oneri);
+        }
+    }
+}
